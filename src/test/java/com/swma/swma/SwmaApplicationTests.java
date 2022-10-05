e0b0540fd40fbe3a2fa56e4440a137787622fc26
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-10-05 15:58:42 +0900

    :tada: 프젝 시작
    dd

---
+++ src/test/java/com/swma/swma/SwmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swma.swma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
