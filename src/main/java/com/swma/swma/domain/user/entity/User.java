1f2fb104b78d83a0f90812573bbc84e6429d1d1d
hee <cmlgml094356@gmail.com>
2022-10-06 01:17:55 +0900

    ♻️  user entity 수정

--- src/main/java/com/swma/swma/domain/user/entity/User.java
+++ src/main/java/com/swma/swma/domain/user/entity/User.java
@@ -1,20 +1,30 @@
 package com.swma.swma.domain.user.entity;
 
 
-import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
 
-import com.swma.swma.domain.user.entity.type.Language;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import com.swma.swma.domain.user.entity.type.Sex;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Getter @Builder
-@AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,9 +39,7 @@ public class User {
     @Column(length = 20) // nullable = false로 변경
     private String name;
 
-    private int year;
-    private int month;
-    private int day;
+    private LocalDate date; // 생년월일
 
     @Enumerated(EnumType.STRING)
     private Sex sex;
@@ -42,13 +50,36 @@ public class User {
     @Column(length = 500) // nullable = false로 변경
     private String description;
 
-    @Enumerated(EnumType.STRING)
-    private Language language;
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String region;
+
+    private LocalDate certifyDate;
 
     private String refreshToken;
 
     public void updateRefreshToken(String refreshToken){
         this.refreshToken = refreshToken;
+    }
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval=true)
+    private List<Language> languages;
+
+    @Builder
+    public User(String userId, String password, String name, int year, int month, int day, Sex sex, String img, String description, String country, String region, LocalDate certifyDate,List<Language> languages) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.date = LocalDate.of(year, month, day);
+        this.sex = sex;
+        this.img = img;
+        this.description = description;
+        this.country = country;
+        this.region = region;
+        this.certifyDate = certifyDate;
+        this.languages = languages;
     }
 
 }
