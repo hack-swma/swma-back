3b61362774198345fd6a0a0bebe46327ca665946
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-10-05 16:47:58 +0900

    :sparkles: 깃킵추가
    엔티티 추가 및 제약조건 추가

--- src/main/java/com/swma/swma/domain/user/entity/User.java
+++ src/main/java/com/swma/swma/domain/user/entity/User.java
@@ -2,10 +2,7 @@ package com.swma.swma.domain.user.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Builder
@@ -14,6 +11,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false,name="user_id",unique = true)
+    private String userId;
+    @Column(length = 60,nullable = false)
     private String password;
     private String name;
     private int year;
