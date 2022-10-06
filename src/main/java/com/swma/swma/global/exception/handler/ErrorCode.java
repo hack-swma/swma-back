6b6c4921cf80695e49d534a030d4952a680f4bc9
hee <cmlgml094356@gmail.com>
2022-10-06 09:38:51 +0900

    🐛 에러 수정

--- src/main/java/com/swma/swma/global/exception/handler/ErrorCode.java
+++ src/main/java/com/swma/swma/global/exception/handler/ErrorCode.java
@@ -11,9 +11,9 @@ public enum ErrorCode {
 	TOKEN_NOT_VALID(401,"토큰이 유효 하지 않습니다."),
 	USER_NOT_FOUND(404, "유저를 찾을 수 없습니다."),
 	USER_ALREADY_EXIST(409,"유저가 이미 존재합니다."),
-  DATE_BAD_REQUEST(400, "생년월일을 잘못 입력했습니다.")
+	DATE_BAD_REQUEST(400, "생년월일을 잘못 입력했습니다."),
 	FAIL_TO_UPLOAD(500,"사진 업로드에 실패했습니다.");
-  
+
 	private final int status;
 	private final String message;
 
